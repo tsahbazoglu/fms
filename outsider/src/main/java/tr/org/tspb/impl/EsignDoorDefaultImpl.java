68522c4ded33909f317b3fd9873bc13333332ecc
tsahbazoglu <tsahbazoglu@tspb.org.tr>
2021-05-09 16:36:10 +0300

    involve factory pattern for form reader

--- outsider/src/main/java/tr/org/tspb/impl/EsignDoorDefaultImpl.java
+++ outsider/src/main/java/tr/org/tspb/impl/EsignDoorDefaultImpl.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 import javax.enterprise.inject.Alternative;
-import tr.org.tspb.dao.MyForm;
+import tr.org.tspb.dao.FmsForm;
 import tr.org.tspb.outsider.EsignDoor;
 
 /**
@@ -31,37 +31,38 @@ public class EsignDoorDefaultImpl implements EsignDoor {
     }
 
     @Override
-    public void initAndShowEsignDlg(List<Map> list, MyForm selectedForm, String widgetVarName, String multiUnique) {
+    public void initAndShowEsignDlg(List<Map> list, FmsForm selectedForm, String widgetVarName, String multiUnique) {
         //
     }
 
     @Override
-    public void initEsignCtrl(MyForm selectedForm, List<Map> listOfCruds, String fullTextSearch, String multiUnique) {
+    public void initEsignCtrl(FmsForm selectedForm, List<Map> listOfCruds, String fullTextSearch, String multiUnique) {
         //
     }
 
     @Override
-    public void initEsignCtrlV2(MyForm selectedForm, String fullTextSearch, String multiUnique) {
+    public void initEsignCtrlV2(FmsForm selectedForm, String fullTextSearch, String multiUnique) {
         //
     }
 
     @Override
-    public void initEsignCtrlV3(MyForm myFormLarge) {
+    public void initEsignCtrlV3(FmsForm myFormLarge) {
         //
     }
 
     @Override
-    public void iniAndShowEsignDlgV1(TreeMap<Integer, String> treeMap, List<Map> listOfCruds, MyForm selectedForm, String widgetVarToBeSignedDialog, String UNIQUE) {
+    public void iniAndShowEsignDlgV1(TreeMap<Integer, String> treeMap, List<Map> listOfCruds,
+            FmsForm selectedForm, String widgetVarToBeSignedDialog, String UNIQUE) {
         //
     }
 
     @Override
-    public void initAndFindEsigns(MyForm selectedForm, String fullTextSearch) {
+    public void initAndFindEsigns(FmsForm selectedForm, String fullTextSearch) {
         //
     }
 
     @Override
-    public void initAndFindEsignsV1(MyForm selectedForm, String fullTextSearch, List<Map> listOfCruds, String multiUnique) {
+    public void initAndFindEsignsV1(FmsForm selectedForm, String fullTextSearch, List<Map> listOfCruds, String multiUnique) {
         //
     }
 
