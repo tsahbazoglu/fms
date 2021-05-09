486526387043f3069302e939eda07861e2d23b64
tsahbazoglu <tsahbazoglu@tspb.org.tr>
2021-05-09 13:48:57 +0300

    involve factory pattern for form reader

--- outsider/src/main/java/tr/org/tspb/outsider/EsignDoor.java
+++ outsider/src/main/java/tr/org/tspb/outsider/EsignDoor.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
-import tr.org.tspb.dao.MyForm;
+import tr.org.tspb.dao.FmsForm;
 
 /**
  *
@@ -21,18 +21,19 @@ public interface EsignDoor extends Serializable {
 
     public String getSignType();
 
-    public void initAndShowEsignDlg(List<Map> list, MyForm selectedForm, String widgetVarName, String multiUnique);
+    public void initAndShowEsignDlg(List<Map> list, FmsForm selectedForm, String widgetVarName, String multiUnique);
 
-    public void initEsignCtrl(MyForm selectedForm, List<Map> listOfCruds, String fullTextSearch, String multiUnique);
+    public void initEsignCtrl(FmsForm selectedForm, List<Map> listOfCruds, String fullTextSearch, String multiUnique);
 
-    public void initEsignCtrlV3(MyForm myFormLarge);
+    public void initEsignCtrlV3(FmsForm myFormLarge);
 
-    public void iniAndShowEsignDlgV1(TreeMap<Integer, String> treeMap, List<Map> listOfCruds, MyForm selectedForm, String widgetVarToBeSignedDialog, String UNIQUE);
+    public void iniAndShowEsignDlgV1(TreeMap<Integer, String> treeMap, List<Map> listOfCruds,
+            FmsForm selectedForm, String widgetVarToBeSignedDialog, String UNIQUE);
 
-    public void initEsignCtrlV2(MyForm selectedForm, String fullTextSearch, String multiUnique);
+    public void initEsignCtrlV2(FmsForm selectedForm, String fullTextSearch, String multiUnique);
 
-    public void initAndFindEsigns(MyForm selectedForm, String fullTextSearch);
+    public void initAndFindEsigns(FmsForm selectedForm, String fullTextSearch);
 
-    public void initAndFindEsignsV1(MyForm selectedForm, String fullTextSearch, List<Map> listOfCruds, String multiUnique);
+    public void initAndFindEsignsV1(FmsForm selectedForm, String fullTextSearch, List<Map> listOfCruds, String multiUnique);
 
 }
