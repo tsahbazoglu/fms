600bb23ffc0bd1b83396add835a07c5f3e713133
tsahbazoglu <tsahbazoglu@tspb.org.tr>
2021-08-31 00:16:39 +0300

    converts bson code-name pair to code

--- datamodel/src/main/java/tr/org/tspb/pojo/MyConstraintFormula.java
+++ datamodel/src/main/java/tr/org/tspb/pojo/MyConstraintFormula.java
@@ -14,29 +14,29 @@ import org.bson.types.ObjectId;
  * @author Telman Şahbazoğlu
  */
 public class MyConstraintFormula {
-    
+
     private final ObjectId id;
     private final EngineType engineType;
     private final String name;
+    private final String relationsPresentation;
     private final String controlFunction;
     private final String resultType;
     private final String relations;
     private final String description;
-    
-    private final Document resultTypeAsBson;
+
     private final int transferOrder;
     private final List checkList;
     private final List<MyConstraintVariable> variables;
     private MyControlResult controlResult;
-    
+
     public MyConstraintFormula(Map contraintFormula) {
         this.id = (ObjectId) contraintFormula.get(MONGO_ID);
         String localEngine = contraintFormula.get(ENGINE).toString();
-        
+
         if (localEngine == null) {
             throw new RuntimeException("Capraz Kontrol Tanımlama Formülü İçin Tanımlanamayan Engine<br/>");
         }
-        
+
         switch (localEngine) {
             case "jeval":
                 this.engineType = EngineType.JEVAL;
@@ -50,28 +50,29 @@ public class MyConstraintFormula {
             default:
                 throw new RuntimeException("Capraz Kontrol Tanımlama Formülü İçin Tanımlanamayan Engine<br/>");
         }
-        
+
+        this.relationsPresentation = contraintFormula.get(RELATIONS_PRESENTATION).toString();
         this.name = contraintFormula.get(NAME).toString();
         this.resultType = contraintFormula.get(RESULT_TYPE).toString();
         this.transferOrder = ((Number) contraintFormula.get(TRANSFER_ORDER)).intValue();
         this.description = contraintFormula.get(DESCRIPTION).toString();
-        
+
         Object localControlFunction = contraintFormula.get(CONTROL_FUNCTION);
         this.controlFunction = localControlFunction == null ? null : ((Code) localControlFunction).getCode();
-        
+
         this.relations = contraintFormula.get(RELATIONS).toString();
-        
+
         variables = new ArrayList<>();
-        
+
         Document variablesDbo = (Document) contraintFormula.get(VARIABLES);
         if (variablesDbo != null) {
             for (String variableKey : variablesDbo.keySet()) {
                 variables.add(new MyConstraintVariable(variablesDbo.get(variableKey), variableKey));
             }
         }
-        
+
         Object check = contraintFormula.get("mySmallLisp");
-        
+
         if (check instanceof String) {
             checkList = new ArrayList();
             checkList.add(check);
@@ -80,86 +81,64 @@ public class MyConstraintFormula {
         } else {
             checkList = new ArrayList();
         }
-
-        //FIXME update db instead of coding here
-        if (null == resultType) {
-            resultTypeAsBson = new Document(CODE, "000").append(NAME, resultType);
-        } else {
-            switch (resultType) {
-                case "Hata":
-                    resultTypeAsBson = new Document(CODE, "000").append(NAME, resultType);
-                    break;
-                case "Uyarı":
-                    resultTypeAsBson = new Document(CODE, "001").append(NAME, resultType);
-                    break;
-                default:
-                    resultTypeAsBson = new Document(CODE, "000").append(NAME, resultType);
-                    break;
-            }
-        }
-        
     }
-    
+
     public EngineType getEngineType() {
         return engineType;
     }
-    
+
     public MyControlResult getControlResult() {
         return controlResult;
     }
-    
+
     public void setControlResult(MyControlResult controlResult) {
         this.controlResult = controlResult;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public int getTransferOrder() {
         return transferOrder;
     }
-    
+
     public String getControlFunction() {
         return controlFunction;
     }
-    
+
     public List<MyConstraintVariable> getVariables() {
         return variables;
     }
-    
+
     public List getCheckList() {
         return Collections.unmodifiableList(checkList);
     }
-    
+
     public ObjectId getId() {
         return id;
     }
-    
+
     public String getResultType() {
         return resultType;
     }
-    
-    public String getResultTypeCode() {
-        return resultTypeAsBson.getString(CODE);
-    }
-    
-    public Document getResultTypeAsBson() {
-        return resultTypeAsBson;
-    }
-    
+
     public String getRelations() {
         return relations;
     }
-    
+
     public String getDescription() {
         return description;
     }
-    
+
+    public String getRelationsPresentation() {
+        return relationsPresentation;
+    }
+
     public enum EngineType {
         JEVAL,
         JAVA_SCRIPT,
         MONGODB_FUNCTION
     }
-    
+
 }
