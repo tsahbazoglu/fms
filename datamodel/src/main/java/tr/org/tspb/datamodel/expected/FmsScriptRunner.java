cab26f6dd533f88c42fd134d58f8eadc83879941
tsahbazoglu <tsahbazoglu@tspb.org.tr>
2021-09-01 23:21:49 +0300

    extends mongodb functions

--- datamodel/src/main/java/tr/org/tspb/datamodel/expected/FmsScriptRunner.java
+++ datamodel/src/main/java/tr/org/tspb/datamodel/expected/FmsScriptRunner.java
@@ -3,6 +3,7 @@ package tr.org.tspb.datamodel.expected;
 import java.util.List;
 import java.util.Map;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import tr.org.tspb.pojo.RoleMap;
 
 /**
@@ -19,10 +20,14 @@ public interface FmsScriptRunner {
 
     public Document findOne(String db, String collection, Document document);
 
+    public List<ObjectId> findObjectIds(String db, String collection, Document document, String projection);
+
     public long count(String db, String collection, Document document);
 
     public Document replaceToDolar(Document document);
 
     public boolean runActionAsDbTableFilterResult(Document actionDoc, RoleMap roleMap, Map filter);
+
+    public List<Document> aggreagate(String db, String table, List<Document> listOfAggrDoc);
 
 }
